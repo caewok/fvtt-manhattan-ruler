60c7f75381664540663f48f9a0c17cada9bd98e2
SolarVh <127591096+SolarVh@users.noreply.github.com>
2023-07-20 17:49:13 -0500

    Update segment.js

--- scripts/segment.js
+++ scripts/segment.js
@@ -8,29 +8,33 @@ import { getSetting, SETTINGS } from "./settings.js";
 
  /*
   * Override Ruler.prototype._computeDistance.
-  * Function to measure the Manhattan distance between two points on the 2D canvas.
-  * @param {boolean} gridSpaces    Base distance on the number of grid spaces moved?
+  * Function to measure the distance between two points on the 2D canvas.
+  * @param {boolean} gridSpaces Base distance on the number of grid spaces moved?
   * @return {Number} The distance of the segment.
   */
 export function _computeDistanceRuler(wrapper, gridSpaces) { // eslint-disable-line no-unused-vars
   if ( getSetting(SETTINGS.ADD_CONTROL) ) {
     const token_controls = ui.controls.controls.find(elem => elem.name === "token");
-    const manhattan_control = token_controls.tools.find(elem => elem.name === "manhattan-distance");
-    if ( !manhattan_control.active ) return wrapper(gridSpaces);
+    const diagonal_control = token_controls.tools.find(elem => elem.name === "manhattan-distance");
+    if ( !diagonal_control.active ) return wrapper(gridSpaces);
   }
 
-  const { size, distance } = canvas.scene.dimensions;
-  const gridConversion = distance / size;
+  const { size, distance } = canvas.scene.dimensions; //size is in pixels and distance is in units
+  //const gridConversion = distance / size; //units per pixel
   let totalDistance = 0;
   const ln = this.segments.length;
   for ( let i = 0; i < ln; i += 1 ) {
-    const s = this.segments[i];
-    const pixel_distance = Math.abs(s.ray.A.x - s.ray.B.x) + Math.abs(s.ray.A.y - s.ray.B.y);
-    const d = pixel_distance * gridConversion;
+    const currentSegment = this.segments[i];
+    const pixel_distanceX = Math.abs(currentSegment.ray.A.x - currentSegment.ray.B.x);
+    const pixel_distanceY = Math.abs(currentSegment.ray.A.y - currentSegment.ray.B.y);
+    const gridDistanceX = pixel_distanceX / size; 
+    const gridDistanceY = pixel_distanceY / size;
+    const gridDistance = Math.floor(1.5*Math.min(gridDistanceX, gridDistanceY)) + Math.abs(gridDistanceX - gridDistanceY)
+    const d = gridDistance * distance;
 
-    s.last = i === (ln - 1);
-    s.distance = d;
+    currentSegment.last = i === (ln - 1);
+    currentSegment.distance = d;
     totalDistance += d;
-    s.text = this._getSegmentLabel(s, totalDistance);
+    currentSegment.text = this._getSegmentLabel(s, totalDistance);
   }
 }
